7a3879f9b5e6fb509ec1f5a559cf9d9836fd7a9a
daniel <rodriguezdaniel048@gmail.com>
2019-11-06 15:49:53 -0500

    Modificado correo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from "./guards/auth.guard";
 import {NologinGuard} from "./guards/nologin.guard";
 const routes: Routes = [
+  
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule) ,canActivate : [AuthGuard]},
 
